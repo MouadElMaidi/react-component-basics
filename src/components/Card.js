82dd3c5100a38926c9db46b0b137f877f6517f62
MouadElMaidi <mouadlmaidi@gmail.com>
2023-08-03 17:46:52 +0100

    Iterate through each element in our key concepts array, return a Card component while passing it the corresponding concept object as a prop

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Card = () => {
-  return <div>Card</div>;
+  return (
+    <div>Card</div>
+    // <li className="concept">
+    //   <img src="TODO: IMAGE" alt="TODO: TITLE" />
+    //   <h2>TODO: TITLE</h2>
+    //   <p>TODO: DESCRIPTION</p>
+    // </li>
+  );
 };
 
 export default Card;
